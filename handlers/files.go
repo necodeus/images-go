b5e20e557267c49dc48eff97deef9472901f354c
necodeus <dawid@necodeo.com>
2024-07-06 13:51:36 +0200

    test

--- handlers/files.go
+++ handlers/files.go
@@ -19,11 +19,16 @@ func GetImage(c *gin.Context) {
 
 	image, err := database.GetImageFromDB(resourceID)
 
+	// print image
+	fmt.Println(image)
+
 	if err != nil {
 		if err == sql.ErrNoRows {
+			fmt.Println("Image not found:", resourceID)
 			c.Writer.WriteHeader(http.StatusNotFound)
 			c.File("./errors/404_not_found.webp")
 		} else {
+			fmt.Println("Error getting image from DB:", err)
 			c.Writer.WriteHeader(http.StatusInternalServerError)
 			c.File("./errors/500_internal_error.webp")
 		}
@@ -34,6 +39,7 @@ func GetImage(c *gin.Context) {
 	filePath := filepath.Join(uploadsDir, resourceID)
 
 	if resolution == "" {
+		fmt.Println("Serving image without resizing:", filePath)
 		c.File(filePath)
 		return
 	}
@@ -41,6 +47,7 @@ func GetImage(c *gin.Context) {
 	width, height, err := utils.ParseResolution(resolution)
 
 	if err != nil {
+		fmt.Println("Error parsing resolution:", err)
 		c.Writer.WriteHeader(http.StatusBadRequest)
 		c.File("./errors/400_invalid_resolution.webp")
 		return
@@ -49,12 +56,14 @@ func GetImage(c *gin.Context) {
 	imageType, err := database.GetImageTypeFromDB(image.TypeName)
 
 	if err != nil {
+		fmt.Println("Error getting image type from DB:", err)
 		c.Writer.WriteHeader(http.StatusInternalServerError)
 		c.File("./errors/500_internal_error.webp")
 		return
 	}
 
 	if !utils.IsValidResolution(imageType.AvailableResolutions, width, height) {
+		fmt.Println("Invalid resolution:", resolution)
 		c.Writer.WriteHeader(http.StatusBadRequest)
 		c.File("./errors/400_invalid_resolution.webp")
 		return
@@ -63,13 +72,19 @@ func GetImage(c *gin.Context) {
 	resizedFilePath := fmt.Sprintf("%s_%dx%d", filePath, width, height)
 
 	if _, err := os.Stat(resizedFilePath); os.IsNotExist(err) {
+		fmt.Println("Generating thumbnail:", resizedFilePath)
+
 		parts := strings.Split(image.TypeName, "/")
+
+		fmt.Println("parts", parts)
+
 		if len(parts) < 2 {
+			fmt.Println("Invalid image type format:", image.TypeName)
 			c.Writer.WriteHeader(http.StatusInternalServerError)
 			c.File("./errors/500_internal_error.webp")
-			fmt.Println("Invalid image type format:", image.TypeName)
 			return
 		}
+
 		fileFormat := parts[1]
 
 		if err := utils.GenerateThumbnail(filePath, resizedFilePath+"."+fileFormat, width, height); err != nil {
