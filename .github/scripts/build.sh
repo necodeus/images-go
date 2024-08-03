7527d9129d0f682d9321499a817b829756bb65b8
Dawid Smulewicz <dawid@necodeo.com>
2024-08-03 13:45:27 +0200

    update release script

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -4,7 +4,7 @@ OS=$1
 ARCH=$2
 
 mkdir -p build
-CGO_ENABLED=0 GOOS=${{ OS }} GOARCH=${{ ARCH }} go build -o ./build/images-${{ OS }}-${{ ARCH }}
+CGO_ENABLED=0 GOOS=${OS} GOARCH=${ARCH} go build -o ./build/images-${OS}-${ARCH}
 cp .env.example ./build/.env
 cp -r errors ./build/errors
 cp init.sql ./build/init.sql
--- .github/scripts/create_release.sh
+++ .github/scripts/create_release.sh
@@ -9,7 +9,7 @@ response=$(curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${TOKEN}" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/repos/${{ REPO_NAME }}/releases \
+    https://api.github.com/repos/${REPO_NAME}/releases \
     -d "{\"tag_name\": \"${TAG_NAME}\", \"name\": \"Release ${TAG_NAME}\", \"draft\": false, \"prerelease\": false}")
 
 release_id=$(echo "$response" | jq -r '.id')
