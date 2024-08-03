38d8449c2374f4650858543fdc813f3b2fe884d3
Dawid Smulewicz <dawid@necodeo.com>
2024-08-03 15:16:14 +0200

    update workflow

--- ci/archive.sh
+++ ci/archive.sh
@@ -3,4 +3,4 @@
 OS=$1
 ARCH=$2
 
-tar -czvf ./build/images-${ARCH}-${ARCH}.tar.gz -C ./build images-${OS}-${ARCH} .env dot.gif init.sql
+tar -czvf ./build/images-${OS}-${ARCH}.tar.gz -C ./build images-${OS}-${ARCH} .env dot.gif init.sql
