da52d889260263f619906da6614d09f1ca170573
Dawid Smulewicz <dawid@necodeo.com>
2024-08-03 14:47:27 +0200

    update workflow

--- ci/create_release.sh
+++ ci/create_release.sh
@@ -12,8 +12,19 @@ response=$(curl -L \
     https://api.github.com/repos/${REPO_NAME}/releases \
     -d "{\"tag_name\": \"${TAG_NAME}\", \"name\": \"Release ${TAG_NAME}\", \"draft\": false, \"prerelease\": false}")
 
-echo "$response"
-
 release_id=$(echo "$response" | jq -r '.id')
+
+if [ -z "$release_id" ]; then
+    release_id=$(curl -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer ${TOKEN}" \
+        https://api.github.com/repos/${REPO_NAME}/releases/tags/${TAG_NAME} | jq -r '.id')
+fi
+
+if [ -z "$release_id" ]; then
+    echo "Failed to create or get release id"
+    exit 1
+fi
+
 
 echo "RELEASE_ID=${release_id}" > release_id
