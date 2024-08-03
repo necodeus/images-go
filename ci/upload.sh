abfe706b23f10ad8830dc9be8674179f67a37e0d
Dawid Smulewicz <dawid@necodeo.com>
2024-08-03 17:03:02 +0200

    update workflow

--- ci/upload.sh
+++ ci/upload.sh
@@ -2,13 +2,14 @@
 
 TOKEN=$1
 REPO_NAME=$2
-TAG_NAME=$3
+RELEASE_ID=$3
 OS=$4
 ARCH=$5
+VERSION=$6
 
 curl -L \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${TOKEN}" \
     -H "Content-Type: application/octet-stream" \
     --data-binary @./build/images-${OS}-${ARCH}.tar.gz \
-    "https://uploads.github.com/repos/${REPO_NAME}/releases/${RELEASE_ID}/assets?name=images-${TAG_NAME}-${OS}-${ARCH}.tar.gz"
+    "https://uploads.github.com/repos/${REPO_NAME}/releases/${RELEASE_ID}/assets?name=images-${VERSION}-${OS}-${ARCH}.tar.gz"
